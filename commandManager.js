841aa5f60d212783382120e9a5171f29f59405e8
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-13 13:15:04 +0300

    feat: add getList function to commandManager

--- commandManager.js
+++ commandManager.js
@@ -2,6 +2,7 @@ import { fs } from './modules/fs/fs.js';
 import { getOsInfo } from './modules/os/getOsInfo.js';
 import { nwd } from './modules/nwd/nwd.js';
 import { calculateHash } from './modules/hash/calcHash.js';
+import { getList } from './modules/ls/getList.js';
 import { showInvalidInputError } from './helpers/showInvalidInputError.js';
 
 export const commandManager = async (command) => {
@@ -13,6 +14,8 @@ export const commandManager = async (command) => {
     getOsInfo(...value);
   } else if (commandKey === 'hash') {
     await calculateHash(...value);
+  } else if (commandKey === 'ls') {
+    await getList();
   } else if (commandKey in nwd) {
     await nwd[commandKey](...value);
   } else {
