04e0c85e1e1b78d965e7c0c99effd4ea2b10b394
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-13 14:17:47 +0300

    refactor: change cpus info and system user name

--- modules/os/getOsInfo.js
+++ modules/os/getOsInfo.js
@@ -1,12 +1,26 @@
-import { cpus, EOL, homedir, hostname, arch } from 'node:os';
+import { cpus, EOL, homedir, userInfo, arch } from 'node:os';
 import { stdout } from 'node:process';
 import { showInvalidInputError, showDirectory } from '../../helpers/index.js';
 
+const getCpusInfo = () => {
+  const result = {};
+  const cpusArr = cpus();
+  const cpusCount = cpusArr.length;
+  result['Total CPUs'] = cpusCount;
+
+  cpusArr.map((cpu, idx) => {
+    const model = cpu.model.trim();
+    const clockRate = (cpu.speed / 1000).toFixed(2);
+    result[`CPU ${idx + 1}`] = { model: model, clockRate: `${clockRate} GHz` };
+  });
+
+  return result;
+};
 const os = {
   '--EOL': () => EOL,
-  '--cpus': () => cpus(),
+  '--cpus': () => getCpusInfo(),
   '--homedir': () => homedir(),
-  '--username': () => hostname(),
+  '--username': () => userInfo().username,
   '--architecture': () => arch(),
 };
 
@@ -14,7 +28,7 @@ export const getOsInfo = (value) => {
   if (value in os) {
     const result = os[value]();
 
-    stdout.write(`${JSON.stringify(result, null, 2)}${EOL}`);
+    stdout.write(`${JSON.stringify(result)}${EOL}`);
     showDirectory();
   } else {
     showInvalidInputError();
