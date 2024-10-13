15987214fe132406c6757c1e12646a1c5891c38b
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-13 13:20:31 +0300

    refactor: add all commands to commandManager

--- commandManager.js
+++ commandManager.js
@@ -1,21 +1,24 @@
 import { fs } from './modules/fs/fs.js';
-import { getOsInfo } from './modules/os/getOsInfo.js';
+import { zip } from './modules/zip/zip.js';
 import { nwd } from './modules/nwd/nwd.js';
 import { calculateHash } from './modules/hash/calcHash.js';
+import { getOsInfo } from './modules/os/getOsInfo.js';
 import { getList } from './modules/ls/getList.js';
 import { showInvalidInputError } from './helpers/showInvalidInputError.js';
 
 export const commandManager = async (command) => {
   const [commandKey, ...value] = command;
 
-  if (commandKey in fs) {
-    await fs[commandKey](...value);
-  } else if (commandKey === 'os') {
+  if (commandKey === 'os') {
     getOsInfo(...value);
   } else if (commandKey === 'hash') {
     await calculateHash(...value);
   } else if (commandKey === 'ls') {
     await getList();
+  } else if (commandKey in fs) {
+    await fs[commandKey](...value);
+  } else if (commandKey in zip) {
+    await zip[commandKey](...value);
   } else if (commandKey in nwd) {
     await nwd[commandKey](...value);
   } else {
