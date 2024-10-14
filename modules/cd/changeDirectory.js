28fcccd834cce7b4caf4c803286abe9495458f4d
Marina Diubailo <marinadiubailo@gmail.com>
2024-10-14 15:09:17 +0300

    refactor: separate nwd module, fix error handling

--- modules/cd/changeDirectory.js
+++ modules/cd/changeDirectory.js
@@ -1,18 +1,26 @@
 import { cwd, chdir } from 'node:process';
 import { resolve } from 'node:path';
 import { access } from 'node:fs/promises';
-import { showInvalidInputError, showDirectory } from '../../helpers/index.js';
+import {
+  showInvalidInputError,
+  showOperationError,
+  showDirectory,
+} from '../../helpers/index.js';
 
 export const changeDirectory = async (path) => {
+  if (!path) {
+    showInvalidInputError();
+    return;
+  }
+
   try {
     const newPath = resolve(path);
     await access(newPath);
 
-    if (newPath === cwd()) return;
+    if (newPath !== cwd()) chdir(newPath);
 
-    chdir(newPath);
     showDirectory();
   } catch (err) {
-    showInvalidInputError();
+    showOperationError();
   }
 };
